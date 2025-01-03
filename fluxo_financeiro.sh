f689ee60bc77454139487e58e5f7effce288dae2
g0ncalo0 <goncaferreira07@gmail.com>
2025-01-03 15:16:09 +0000

    Aqui criei a função que permite fornecer o menu interativo para o usuário escolher a opção que quer realizar, no final testei tudo e consegui guardar os dados no ficheiro dados_financeiros.txt, e consegui criar a partir desse ficheiro um ficheiro csv para depois abrir em Excel.

--- fluxo_financeiro.sh
+++ fluxo_financeiro.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LANG=pt_BR.UTF-8
+
 #Ficheiro onde os dados vão ser armazenados
 FICHEIRO_DADOS="dados_financeiros.txt"
 
@@ -13,10 +15,10 @@ init_ficheiro(){
 #Função para adicionar receitas ou despesas
 adicionar_transacao(){
     echo "=== Adicionar Transação ==="
-    read -p "Tipo (Receita ou Despesa): "
-    read -p "Categoria (ex:Alimentação, Salário; etc..): "
-    read -p "Valor: "
-    read -p "Descrição: "
+    read -p "Tipo (Receita ou Despesa): " Tipo
+    read -p "Categoria (ex:Alimentação, Salário; etc..): " Categoria
+    read -p "Valor: " Valor
+    read -p "Descrição: " Descricao
 
     if [["$Tipo" != "Receita" && "$Tipo" != "Despesa" ]]; then
         echo "Erro: Tipo inválido, use 'Receita' ou 'Despesa'."
@@ -24,7 +26,7 @@ adicionar_transacao(){
     fi
 
 #Registar no ficheiro
-echo "$(date '+%A-%m-%d') | $Tipo | $Categoria | $Valor | $Descricao" >> "$FICHEIRO_DADOS"
+echo "$(date '+%d-%m-%Y') | $Tipo | $Categoria | $Valor | $Descricao" >> "$FICHEIRO_DADOS"
 echo "Transação adicionada com sucesso!"
 
 }
@@ -35,10 +37,11 @@ ver_saldo(){
     Receita=$(awk -F';' '$2 == "Receita" {sum += $4} END {print sum}' "$FICHEIRO_DADOS")
     Despesa=$(awk -F';' '$2 == "Despesa" {sum += $4} END {print sum}' "$FICHEIRO_DADOS")
     Saldo=$(echo "$Receita - $Despesa" | bc)
+    
 
-    echo "Receitas: € $Receita"
-    echo "Despesas: € $Despesa"
-    echo "Saldo: € $Saldo"
+    echo "Receitas: $Receita€"
+    echo "Despesas: $Despesa€"
+    echo "Saldo: $Saldo€"
 }
 
 #Função para exportar os dados para CSV
@@ -55,13 +58,28 @@ exportar_csv(){
     fi
 }
 
+#Função do Menu principal
+menu(){
+    while true; do
+        echo "=== Simulador de fluxo financeiro ==="
+        echo "1. Adicionar Transação"
+        echo "2. Ver Saldo Atual"
+        echo "3. Exportar para ficheiro CSV"
+        echo "4. Sair"
+        read -p "Escolha uma opção: " Opcao
 
+        case "$Opcao" in
+            1) adicionar_transacao ;;
+            2) ver_saldo ;;
+            3) exportar_csv ;;
+            4) echo "A sair..."; break ;;
+            *) echo "Opção inválida!" ;;
+        esac
 
+    done
+}
 
-
-
-
-
-
+#Inicializar o ficheiro e executar o Menu principal
 init_ficheiro
+menu
 
